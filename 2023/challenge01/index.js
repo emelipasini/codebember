3c6690111d7623a6eba5209f19b670ecdc945e16
Emelí Pasini <emelipasini@outlook.com>
2023-11-24 20:04:57 +0100

    corrigo indentacion en el challenge 01

--- 2023/challenge01/index.js
+++ 2023/challenge01/index.js
@@ -1,26 +1,26 @@
 fs = require("fs");
 
 fs.readFile("./message.txt", "utf8", function (err, data) {
-  if (err) {
-    return console.log(err);
-  }
-
-  data = data.toLowerCase();
-  const words = data.split(" ");
-
-  let wordsCount = [];
-  for (const word of words) {
-    if (typeof wordsCount[word] === "undefined") {
-      wordsCount[word] = 1;
-    } else {
-      wordsCount[word]++;
+    if (err) {
+        return console.log(err);
     }
-  }
 
-  let result = "";
-  for (const word in wordsCount) {
-    result += `${word}${wordsCount[word]}`;
-  }
+    data = data.toLowerCase();
+    const words = data.split(" ");
 
-  console.log(result);
+    let wordsCount = [];
+    for (const word of words) {
+        if (typeof wordsCount[word] === "undefined") {
+            wordsCount[word] = 1;
+        } else {
+            wordsCount[word]++;
+        }
+    }
+
+    let result = "";
+    for (const word in wordsCount) {
+        result += `${word}${wordsCount[word]}`;
+    }
+
+    console.log(result);
 });
